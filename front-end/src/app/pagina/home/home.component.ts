88a69012f9f1c3b7cc09deb17fc35b8c79362bfe
Eduardo Hermes Becker <eduardohermesb@gmail.com>
2024-12-12 17:25:36 -0300

    Implementação de novos componentes

--- front-end/src/app/pagina/home/home.component.ts
+++ front-end/src/app/pagina/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { PesquisaComponent } from '../../components/pesquisa/pesquisa.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent, FooterComponent],
+  imports: [CardComponent, FooterComponent, PesquisaComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
